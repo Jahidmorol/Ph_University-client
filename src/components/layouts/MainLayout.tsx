ee61bc341760d6bb365ad6a6c7ec105e21f009e5
jahid <jahidmorol2@gmail.com>
2024-01-24 12:13:24 +0600

    implant sidebar logic and other

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,67 +1,13 @@
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
+import Sidebar from "./Sidebar";
 
-const { Header, Content, Footer, Sider } = Layout;
-
-// const items: MenuProps["items"] = [
-//   {
-//     key: "1",
-//     label: "Dashboard",
-//   },
-//   {
-//     key: "2",
-//     label: "Profile",
-//   },
-//   {
-//     key: "3",
-//     label: "UserManagement",
-//     children: [
-//       {
-//         key: "11",
-//         label: "Create Admin",
-//       },
-//       {
-//         key: "22",
-//         label: "Create Student",
-//       },
-//     ],
-//   },
-// ];
+const { Header, Content, Footer } = Layout;
 
 const MainLayout = () => {
   return (
     <Layout style={{ height: "100vh" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="demo-logo-vertical" />
-        <div
-          style={{
-            color: "white",
-            textAlign: "center",
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h1>PH University</h1>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={adminSidebarItems}
-        />
-      </Sider>
+      <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: "24px 16px 0" }}>
