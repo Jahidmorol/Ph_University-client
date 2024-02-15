eb07f4392450f85df6e684594b123c8d8f1dc36a
jahid <jahidmorol2@gmail.com>
2024-02-15 07:52:58 +0600

    implant create student page with some funtionalitys

--- src/pages/admin/CreateStudent.tsx
+++
@@ -1,9 +0,0 @@
-const CreateStudent = () => {
-  return (
-    <div>
-      <h2>this is CreateStudent page</h2>
-    </div>
-  );
-};
-
-export default CreateStudent;
---
+++ src/redux/features/admin/userManagement.api.ts
@@ -0,0 +1,15 @@
+import { baseApi } from "../../api/baseApi";
+
+const academicManagementApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    addStudent: builder.mutation({
+      query: (data) => ({
+        url: "/users/create-student",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useAddStudentMutation } = academicManagementApi;
