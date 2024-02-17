8e4b1ce621f8e3743c515756fa5603a82dcb4bca
jahid <jahidmorol2@gmail.com>
2024-02-17 18:19:56 +0600

    implant update mark modal for student and almost done

--- src/pages/faculty/MyStudents.tsx
+++ src/pages/faculty/MyStudents.tsx
@@ -1,10 +1,12 @@
-import { Button, Modal, Table, TableColumnsType } from "antd";
+import { Button, Modal, Table } from "antd";
 import { useState } from "react";
 import PHForm from "../../components/form/PHForm";
-import PHSelect from "../../components/form/PHSelect";
 import PHInput from "../../components/form/PHInput";
 import { useParams } from "react-router-dom";
-import { useGetAllFacultyCoursesQuery } from "../../redux/features/faculty/facultyCourses.api";
+import {
+  useAddMarkMutation,
+  useGetAllFacultyCoursesQuery,
+} from "../../redux/features/faculty/facultyCourses.api";
 
 const MyStudents = () => {
   const { registerSemesterId, courseId } = useParams();
@@ -43,11 +45,7 @@ const MyStudents = () => {
       title: "Action",
       key: "x",
       render: (item) => {
-        return (
-          <div>
-            <Button>Update</Button>
-          </div>
-        );
+        return <AddMarksModal studentInfo={item} />;
       },
     },
   ];
@@ -59,6 +57,58 @@ const MyStudents = () => {
       columns={columns}
       dataSource={tableData}
     />
+  );
+};
+
+const AddMarksModal = ({ studentInfo }) => {
+  console.log(studentInfo);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [addMark] = useAddMarkMutation();
+
+  const handleSubmit = async (data) => {
+    const studentMark = {
+      semesterRegistration: studentInfo.semesterRegistration,
+      offeredCourse: studentInfo.offeredCourse,
+      student: studentInfo.student,
+      courseMarks: {
+        classTest1: Number(data.classTest1),
+        midTerm: Number(data.midTerm),
+        classTest2: Number(data.classTest2),
+        finalTerm: Number(data.finalTerm),
+      },
+    };
+
+    const res = await addMark(studentMark);
+
+    console.log(res);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={showModal}>Add Faculty</Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <PHForm onSubmit={handleSubmit}>
+          <PHInput type="text" name="classTest1" label="Class Test 1" />
+          <PHInput type="text" name="classTest2" label="Class Test 2" />
+          <PHInput type="text" name="midTerm" label="Midterm" />
+          <PHInput type="text" name="finalTerm" label="Final" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Modal>
+    </>
   );
 };
 
